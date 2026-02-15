1ceeff36cba8b59069100b2e22a613a588ac6009
Leo <81168288+Zare22@users.noreply.github.com>
2026-02-15 14:41:24 +0100

    Initial KMP project setup

---
+++ shared/src/commonMain/kotlin/com/leo/rickandmortyexplorer/Greeting.kt
@@ -0,0 +1,9 @@
+package com.leo.rickandmortyexplorer
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
